4c5eec67ec7545e50cd15d9763b39acda6676403
greg <gwprill@gmail.com>
2017-05-28 23:30:07 -0700

    formatting

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 var user = require('./user');
 
 module.exports = {
-  configure: function(app) {
-    //when a username and password is posted to this url check it
-    app.post('/login/', function(req, res) {
-      user.login(req.body.username, req.body.password, req, res);
-    });
-  }
+	configure: function(app) {
+		//when a username and password is posted to this url check it
+		app.post('/login/', function(req, res) {
+			user.login(req.body.username, req.body.password, req, res);
+		});
+	}
 };
--- user.js
+++ user.js
@@ -5,35 +5,35 @@ var jwt = require('jsonwebtoken');
 var userSecret = 'T9mjPBCY2tfJSSQaNDQAsCFz';
 
 function User() {
-  //given username and password return a JSON web token with the users info if username and password match otherwise give a meaningful error message
-  this.login = function(username, password, req,res) {
-	MongoClient.connect(mongoUrl, function(err, db) {
-	  if (err) throw err;
-	  var query = { username: username };
-	  //get all users with the provided username
-	  db.collection('users').find(query).toArray(function(err, result) {
-	    if (err) throw err;
-	    //if result is empty then there is no user with that name
-	    if(result.length==0){
-	    	res.send({error: 'No user by that name.'});
-	    } else {
-	    	bcrypt.compare(password, result[0].password.replace('$2y$','$2a$'), function (err,bverified) {
-	    		if(bverified){
-	    			//let's not send any info about the password just to be safe
-	    			result[0].password = '';
-	    			//return our JSON web token signed by our secret which we will use to verify when the user tries to access data
-	    			res.send({ token : jwt.sign({
-								  data: result[0]
-								}, userSecret, { expiresIn: 60 * 60 * 24 })
-	    			});
-	    		} else {
-	    			res.send('Password is incorrect');
-	    		}
-	    	});
-	    }
-	    db.close();
-	  });
-	});
-  };
+	//given username and password return a JSON web token with the users info if username and password match otherwise give a meaningful error message
+	this.login = function(username, password, req,res) {
+		MongoClient.connect(mongoUrl, function(err, db) {
+			if (err) throw err;
+			var query = { username: username };
+			//get all users with the provided username
+			db.collection('users').find(query).toArray(function(err, result) {
+				if (err) throw err;
+				//if result is empty then there is no user with that name
+				if(result.length==0){
+					res.send({error: 'No user by that name.'});
+				} else {
+					bcrypt.compare(password, result[0].password.replace('$2y$','$2a$'), function (err,bverified) {
+						if(bverified){
+							//let's not send any info about the password just to be safe
+							result[0].password = '';
+							//return our JSON web token signed by our secret which we will use to verify when the user tries to access data
+							res.send({ token : jwt.sign({
+										  data: result[0]
+										}, userSecret, { expiresIn: 60 * 60 * 24 })
+							});
+						} else {
+							res.send('Password is incorrect');
+						}
+					});
+				}
+				db.close();
+			});
+		});
+	};
 }
 module.exports = new User();
